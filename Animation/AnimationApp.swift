f44dc96665854445fb8d0cbb075adf2d8c0c4c02
Adarsh Ranjan <adarsh0ranjan@gmail.com>
2025-12-28 11:18:31 +0530

    basic

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Adarsh Ranjan on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
